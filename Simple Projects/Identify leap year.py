381d2d0b396b286e94e3aa3cb656ed34a7dbfaf9
Azar Ibrahimli <azer.ibrahimli.98@gmail.com>
2022-01-16 20:34:03 +0400

    Add files via upload

---
+++ Simple Projects/Identify leap year.py
@@ -0,0 +1,12 @@
+#Day3
+year=int(input("Which year do you want to check? : "))
+if year%4==0:
+    if year%100==0:
+        if year%400==0:
+            print(f"{year} is leap year")
+        else:
+            print(f"{year} is not leap year")
+    else:
+         print(f"{year} is leap year")
+else:
+    print(f"{year} is not leap year")
