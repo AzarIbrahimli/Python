a1e67590fa131972827a4144cbb48e9b84a1fee4
Azar Ibrahimli <azer.ibrahimli.98@gmail.com>
2022-01-19 23:10:59 +0400

    Delete Average Height.py

--- Exercises/Average Height.py
+++
@@ -1,9 +0,0 @@
-height=input("Enter a list of student heights,pelase : ").split()
-total_height=0
-for n in range(0, len(height)):
-    total_height=total_height+int(height[n])
-average=total_height/(n+1)
-print(f"Average is {round(average)}")         
-
-
-    
