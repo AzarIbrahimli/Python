fca39c351ee3ae2bd268a8e7cffa146a1fa0cc0c
Azar Ibrahimli <azer.ibrahimli.98@gmail.com>
2022-01-22 23:33:22 +0400

    Add files via upload

---
+++ Exercises/Life in weeks/Life in weeks.py
@@ -0,0 +1,6 @@
+age=int(input("What is your current age? "))
+remained_age=90-age
+result_days= remained_age*365
+result_weeks=remained_age*52
+result_month=remained_age*12
+print(f"You have {result_days} days, {result_weeks} weeks and {result_month} months left")
